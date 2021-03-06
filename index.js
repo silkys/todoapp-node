de48a20b1ab0b63e2ae68a610223e45034a3b546
silkys <silky.sinha09@gmail.com>
2021-03-06 05:11:42 -0800

    my first todo app

---
+++ index.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+
+const mongoose = require("mongoose");
+
+//models
+const TodoTask = require("./models/TodoTask");
+
+dotenv.config();
+
+
+app.use("/static", express.static("public"));
+
+app.use(express.urlencoded({extented:true}));
+//view engine configuration
+app.set("view engine", "ejs");
+
+
+//connection to db
+mongoose.set("useFindAndModify", false);
+
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
+
+
+
+
+app.get("/", async (req, res) => {
+    const tasks = await TodoTask.find({});
+    console.log(tasks);
+      res.render("todo.ejs", { todoTasks: tasks });
+});
+
+
+//POST METHOD
+app.post('/',async (req, res) => {
+    const todoTask = new TodoTask({
+    content: req.body.content
+    });
+    try {
+    await todoTask.save();
+    res.redirect("/");
+    } catch (err) {
+    res.redirect("/");
+    }
+    });
+
+
+
+    //UPDATE
+app
+.route("/edit/:id")
+.get((req, res) => {
+const id = req.params.id;
+TodoTask.find({}, (err, tasks) => {
+res.render("todoEdit.ejs", { todoTasks: tasks, idTask: id });
+});
+})
+.post((req, res) => {
+const id = req.params.id;
+TodoTask.findByIdAndUpdate(id, { content: req.body.content }, err => {
+if (err) return res.send(500, err);
+res.redirect("/");
+});
+});
+
+//DELETE
+app.route("/remove/:id").get((req, res) => {
+    const id = req.params.id;
+    TodoTask.findByIdAndRemove(id, err => {
+    if (err) return res.send(500, err);
+    res.redirect("/");
+    });
+    });
+    
+    
+
+
+console.log("Connected to db!");
+app.listen(3012, () => console.log("Server Up and running"));
+});
